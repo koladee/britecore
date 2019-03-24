81073b695471ca265cb968ab3af53220181ba354
Kolade Emmanuel <48717622+koladee@users.noreply.github.com>
2019-03-24 07:09:13 +0100

    Update test_cases.py

--- tests/test_cases.py
+++ tests/test_cases.py
@@ -20,7 +20,7 @@ class Bucketlist(unittest.TestCase):
                         'title': 'Request title',
                         'description': "The breakdown of request goes here",
                         'client_id': 1,
-                        'product': 2,    
+                        'product': 3,    
                         'target_date': '2018-11-06',
                         'priority': 4
                     }
@@ -29,13 +29,13 @@ class Bucketlist(unittest.TestCase):
         with self.app.app_context():
             db.create_all()
 
-    def test_create_client_and_request(self):
+    def create_client_and_request(self):
         client = Client(name="Test_client")
         data = [Requests(
             title=f'Request title {index+1}',
             description=f'The breakdown of request goes here {index+1}',
             client_id=1,
-            product=2,    
+            product=1,    
             target_date=date(2019,03,23),
             priority=4
          ) for index, product in Productarea]
@@ -43,33 +43,33 @@ class Bucketlist(unittest.TestCase):
         db.session.add_all(data)
         db.session.commit()
 
-    def test_create_new_request(self):
+    def create_new_request(self):
         res = self.client().post('/request/new', data=self.request)
         self.assertEqual(Requests.query.count(), 1)
         self.assertEqual(res.status_code, 302)
 
     def test_if_existing_requests are reorderd(self):
-        self.test_create_client_and_request()
+        self.create_client_and_request()
         self.assertEqual(Requests.query.count(), 4)
         self.assertEqual(Requests.query.get(1).title, 'title 1')
         self.assertEqual(Requests.query.get(1).priority, 1)
         res = self.client().post('/request/new', data=self.request)
-        self.assertEqual(Requests.query.get(1).priority, 2)
+        self.assertEqual(Requests.query.get(1).priority, 4)
         self.assertEqual(Requests.query.get(2).priority, 3)
-        self.assertEqual(Requests.query.get(3).priority, 4)
+        self.assertEqual(Requests.query.get(3).priority, 3)
         self.assertEqual(Requests.query.get(4).priority, 5)
-        self.assertEqual(Requests.query.get(5).priority, 1)
+        self.assertEqual(Requests.query.get(5).priority, 4)
         self.assertEqual(Requests.query.count(), 5)
         self.request.update(priority=3)
         res = self.client().post('/request/new', data=self.request)
         self.assertEqual(Requests.query.get(6).priority, 3)
-        self.assertEqual(Requests.query.get(2).priority, 4)
+        self.assertEqual(Requests.query.get(2).priority, 2)
         self.assertEqual(Requests.query.get(3).priority, 5)
         self.assertEqual(Requests.query.count(), 6)
         self.assertEqual(res.status_code, 302)
 
     def test_for_no_priority_duplicate(self):
-        self.test_create_client_and_request()
+        self.create_client_and_request()
         self.assertEqual(Requests.query.count(), 4)
         self.assertEqual(Requests.query.get(1).title, 'title 1')
         self.assertEqual(Requests.query.get(1).priority, 1)
@@ -83,7 +83,7 @@ class Bucketlist(unittest.TestCase):
         self.assertEqual(Requests.query.count(), 5)
     
     def test_if_priority_of_requests_does_not_reordered_if_priority_on_new_request_is_not_set(self):
-        self.test_create_client_and_request()
+        self.create_client_and_request()
         self.assertEqual(Requests.query.count(), 4)
         self.assertEqual(Requests.query.get(1).title, 'title 1')
         self.assertEqual(Requests.query.get(1).priority, 1)
